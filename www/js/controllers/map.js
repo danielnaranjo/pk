af726c2566ac9c4d4249ed6055a04ae24e177faa
Daniel Naranjo <dnaranjo@gmail.com>
2017-02-16 18:03:44 -0300

    Fix

--- www/js/controllers/map.js
+++ www/js/controllers/map.js
@@ -6,17 +6,12 @@ app.controller('MapaCtrl', function($scope, $timeout, $ionicLoading, $http, Exch
   $ionicLoading.show();
   $scope.zoom=5;
 
-  //$scope.lat = -34.603704;
-  //$scope.long = -58.381613;
   $scope.ubicar = function(){
     $log.log('ubicar');
     geoService.getPosition().then(function(position) {
       $scope.coords = position.coords;
       $log.info('geoService', $scope.coords.latitude,$scope.coords.longitude);
       $scope.geolocalization = $scope.coords.latitude.toFixed(3)+','+$scope.coords.longitude.toFixed(3);
-
-      $scope.lat = $scope.coords.latitude;
-      $scope.long = $scope.coords.longitude;
       //http://ngmap.github.io/#/!custom-marker-ng-repeat.html
       $scope.map = {
         center: {
@@ -24,7 +19,17 @@ app.controller('MapaCtrl', function($scope, $timeout, $ionicLoading, $http, Exch
           longitude: $scope.long
         }
       };
-      $log.debug('geolocalization', $scope.lat, $scope.long);
+      //remoteServer.getData('points/null/'+$scope.coords.latitude+'/'+$scope.coords.longitude)
+      remoteServer.getData('points')
+      .success(function(data) {
+          $scope.timeline=data.results;
+          $scope.zoom=16;
+          $ionicLoading.hide();
+      })
+      .error(function(){
+        $ionicLoading.hide();
+        $log.error('Error MapaCtrl mapa()');
+      });
     }, function(err) {
       $ionicLoading.hide();
       $log.error('MapaCtrl getCurrentPosition error: ', err.message);
@@ -34,16 +39,6 @@ app.controller('MapaCtrl', function($scope, $timeout, $ionicLoading, $http, Exch
   $scope.mapa = function(){
     $scope.$on('$ionicView.enter', function(){
       $scope.ubicar();
-      remoteServer.getData('points')
-      .success(function(data) {
-          $scope.timeline=data.data;
-          $scope.zoom=16;
-          $ionicLoading.hide();
-      })
-      .error(function(){
-        $ionicLoading.hide();
-        $log.error('Error MapaCtrl mapa()');
-      });
     });
   }; // mapa
 
--- www/js/controllers/timeline.js
+++ www/js/controllers/timeline.js
@@ -1,28 +1,55 @@
-app.controller('TimelineCtrl', function($scope, Exchange, $http, $ionicLoading, Config, $log, remoteServer, $localstorage) {
+app.controller('TimelineCtrl', function($scope, Exchange, $http, $ionicLoading, Config, $log, remoteServer, $localstorage, geoService) {
 
 // Exchange's services
-  $scope.lat = Exchange.data.lat;
-  $scope.long = Exchange.data.long;
-  $log.info('Exchange at timeline', angular.toJson(Exchange.data));
+  //$scope.lat = Exchange.data.lat;
+  //$scope.long = Exchange.data.long;
+  //$log.info('Exchange at timeline', angular.toJson(Exchange.data));
+
+$scope.ubicar = function(){
+    $log.log('ubicar');
+    geoService.getPosition().then(function(position) {
+      $scope.coords = position.coords;
+      $log.info('geoService', $scope.coords.latitude,$scope.coords.longitude);
+      $scope.geolocalization = $scope.coords.latitude.toFixed(3)+','+$scope.coords.longitude.toFixed(3);
+
+      $scope.lat = $scope.coords.latitude;
+      $scope.long = $scope.coords.longitude;
+      //http://ngmap.github.io/#/!custom-marker-ng-repeat.html
+      $scope.map = {
+        center: {
+          latitude: $scope.lat,
+          longitude: $scope.long
+        }
+      };
+      $log.debug('geolocalization', $scope.lat, $scope.long);
+    }, function(err) {
+      $ionicLoading.hide();
+      $log.error('MapaCtrl getCurrentPosition error: ', err.message);
+    });
+  };
 
   $scope.getAll = function(){
     $ionicLoading.show();
-
+    $scope.ubicar();
+    //remoteServer.getData('points/null/'+$scope.lat+'/'+$scope.long)
     remoteServer.getData('points')
     .success(function(data) {
+      $ionicLoading.hide();
+      $scope.timeline = data.results;
       var points = [];
       for(var i=0; i<data.results.length+1; i++){
+        //$log.debug(angular.toJson(data.results[i]));
         var point = {
-            id:             data.results[i].notification_id||UUIDjs.create().toString(),
+            //id:             data.results[i].geofence_id,
             latitude:       data.results[i].latitude||0,
             longitude:      data.results[i].longitude||0,
             radius:         data.results[i].radius||100,
             transitionType: data.results[i].notification.transitionType||1,
             notification: {
-              id:             1,
+              id:             data.results[i].notification.notification_id,
               title:          'Pooock!',// si paga mas, tiene mensaje personalizado!
               text:           data.results[i].notification.message||'Tenemos una promo para ti!',
-              vibration:      data.results[i].notification.vibration||[0], // si paga mas, tiene vibracion personalizado!
+              vibration:      [0], // si paga mas, tiene vibracion personalizado! -> data.results[i].notification.vibration||
               smallIcon:      'res://icon', // transparente
               icon:           'file://img/icono.png',
               openAppOnClick: data.results[i].notification.openAppOnClick||true,
@@ -33,7 +60,6 @@ app.controller('TimelineCtrl', function($scope, Exchange, $http, $ionicLoading, 
         };
         points.push(point);
       }
-      $ionicLoading.hide();
       $log.log('data @timeline', data.results.length);
       $localstorage.clear('points');
       $localstorage.setObject('points', data.results);
